311f15a2ed0f38c41a27b26618d410de24febf62
JIrleidySantos <jirleidysantosm@gmail.com>
2024-11-05 15:05:04 -0500

    welin

---
+++ src/main/java/ec/edu/espoch/tegnologuia/Tegnologuia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.tegnologuia;
+
+/**
+ *
+ * @author mundo
+ */
+public class Tegnologuia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
